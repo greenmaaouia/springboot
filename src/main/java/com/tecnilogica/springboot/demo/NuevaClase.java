2dc766e05a34da49ce6abbe6333874245a431a23
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2014-11-28 13:52:50 +0100

    Ejemplo

---
+++ src/main/java/com/tecnilogica/springboot/demo/NuevaClase.java
@@ -0,0 +1,13 @@
+package com.tecnilogica.springboot.demo;
+
+public class NuevaClase {
+
+	public static void main(String[] args) {
+		System.out.print("linea1");
+		System.out.print("linea2");
+		System.out.print("linea3");
+		System.out.print("linea4");
+		System.out.print("linea5");
+	}
+	
+}
